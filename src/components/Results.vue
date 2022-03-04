a1b820e193c2edc2a7a9ab025345a6d52f65c955
admin <fatimamahlaba@gmail.com>
2022-03-04 15:00:55 +0200

    last update

---
+++ src/components/Results.vue
@@ -0,0 +1,34 @@
+<template>
+  <p>Reaction time: {{score}} ms</p>
+  <p class="speed">{{speed}}</p>
+</template>
+
+<script>
+export default {
+props: ['score'],
+data() {
+  return {
+    speed: null
+  }
+},
+mounted() {
+  if(this.score < 350){
+    this.speed = "Very fast"
+  }
+  else if (this.score < 500){
+    this.speed = "Relatively fast"
+  }
+  else {
+    this.speed ="Not so fast"
+  }
+}
+}
+</script>
+
+<style>
+.speed{
+  font-size: 1.4rem;
+  color: #0faf82;
+  font-weight: bold;
+}
+</style>
